b1c3cb6e8969bcbe3f7c1ca33b6b1e9745ee1629
nobody5050 <32989720+nobody5050@users.noreply.github.com>
2019-05-18 17:15:56 -0500

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -1,5 +1,9 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+
+client.on("ready", () => {
+  console.log("I am ready!");
+});
  
 // Set the prefix
 let prefix = "!";
