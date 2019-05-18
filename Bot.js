08fd76cee86eddecca991850878fc1ada1e7c84b
nobody5050 <32989720+nobody5050@users.noreply.github.com>
2019-05-18 16:05:20 -0500

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -22,6 +22,14 @@ client.on('message', message => {
 
 });
 
+client.on('guildMemberAdd', member => {
+  // Send the message to a designated channel on a server:
+  const channel = member.guild.channels.find(ch => ch.name === 'member-log');
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send(`Welcome to the server, ${member}`);
+});
  
 
 // THIS  MUST  BE  THIS  WAY
