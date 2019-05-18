c614b19e8734e9d77829d5d993551288665b7214
nobody5050 <32989720+nobody5050@users.noreply.github.com>
2019-05-18 17:06:50 -0500

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -2,6 +2,10 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
  
+
+const args = message.content.slice(prefix.length).trim().split(/ +/g);
+const command = args.shift().toLowerCase();
+
 client.on("message", (message) => {
   if (!message.content.startsWith(config.prefix) || message.author.bot) return;
  
