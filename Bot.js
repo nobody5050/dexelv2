cac4f37863e5bf4502722957365b124a4e62559b
nobody5050 <32989720+nobody5050@users.noreply.github.com>
2019-05-18 16:52:30 -0500

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -5,7 +5,7 @@ client.on("ready", () => {
   console.log("I am ready!");
 });
  
-const prefix = "!";
+const prefix = "r";
 client.on("message", (message) => {
   // Exit and stop if it's not there
   if (!message.content.startsWith(prefix)) return;
