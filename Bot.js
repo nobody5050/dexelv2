7e1f5ca3473e0e870578d6bb7e9df1b774ac6c4c
nobody5050 <32989720+nobody5050@users.noreply.github.com>
2019-05-19 14:49:53 -0500

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -19,4 +19,14 @@ client.on("message", (message) => {
   }
 });
 
+client.on('guildMemberAdd', member => {
+  // Send the message to a designated channel on a server:
+  const channel = member.guild.channels.find(ch => ch.name === 'member-log');
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send(`Welcome to the server, ${member}`);
+});
+
+
 client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
