3740574fa9fe3b6f64b85b1a70833476124822cc
nobody5050 <32989720+nobody5050@users.noreply.github.com>
2019-05-19 16:41:09 -0500

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -21,6 +21,15 @@ client.on("message", (message) => {
   client.users.get("477907717273681952").send("[message1, line 1], [message 1 line 2]");
     message.channel.send("message sent.");
   }
+  bot.on('message', async message => {
+    if (message.channel.type === 'dm'){ 
+        console.log(message.content);
+        if(message.content === "something"){
+            return await message.channel.send("Hi!");
+        }
+        return;
+    }
+  });
 });
 
 client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
