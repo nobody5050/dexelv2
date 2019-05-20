0147f80362f7cdcf169e159d5138dcb059c20ea9
nobody5050 <32989720+nobody5050@users.noreply.github.com>
2019-05-20 06:20:49 -0500

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -20,7 +20,7 @@ client.on("message", (message) => {
   if (message.content.startsWith(prefix + "mk")) {
   client.users.get("477907717273681952").send("[message1, line 1], [message 1 line 2]");
     message.channel.send("message sent.");
-  }
+  } else
   if (message.channel.type === 'dm'){ 
         console.log(message.content);
         return;
